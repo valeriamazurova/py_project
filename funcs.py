2594ad7dbf1de7f6a56d038a03fb4880ec926201
Valeria Mazurova <valeryfox90@gmail.com>
2023-12-03 21:10:32 +0500

    Добавила фунцию минус

--- funcs.py
+++ funcs.py
@@ -4,3 +4,4 @@ def funcs(num1, num2):
 
 def funcs_minus(num1, num2):
     return num1 - num2
+
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
-from funcs import funcs
+from funcs import funcs, funcs_minus
 
 print(funcs(2, 3))
+print(funcs_minus(10, 5))
